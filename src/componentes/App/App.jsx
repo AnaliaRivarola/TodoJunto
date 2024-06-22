acd896ce2dbffa1aa22a718828b07576eea21f32
AnaliaRivarola <80989474+AnaliaRivarola@users.noreply.github.com>
2024-06-22 13:44:32 -0400

    Poniendo todo junto

--- src/componentes/App/App.jsx
+++ src/componentes/App/App.jsx
@@ -1,42 +1,39 @@
 import React, { Component } from "react";
 import PersonCard from "../PersonCard/PersonCard";
 import "./App.css";
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      persons: [
+        { firstName: "Jane", lastName: "Doe", age: 45, hairColor: "Black" },
+        { firstName: "John", lastName: "Smith", age: 88, hairColor: "Brown" },
+        { firstName: "Millard", lastName: "Fillmore", age: 50, hairColor: "Brown" },
+        { firstName: "Maria", lastName: "Smith", age: 62, hairColor: "Brown" }
+      ]
+    };
+  }
+
+  incrementarEdad = (index) => {
+    const updatedPersons = [...this.state.persons];
+    updatedPersons[index].age += 1;
+    this.setState({ persons: updatedPersons });
   }
 
   render() {
     return (
       <>
-        <PersonCard
-          firstName={"Jane"}
-          lastName={"Done"}
-          age={"45"}
-          hairColor={"Black"}
-        />
-
-        <PersonCard
-          firstName={"John"}
-          lastName={"Smith"}
-          age={"88"}
-          hairColor={"Brown"}
-        />
-
-        <PersonCard
-          firstName={"Millard"}
-          lastName={"Fillmore"}
-          age={"50"}
-          hairColor={"Brown"}
-        />
-
-        <PersonCard
-          firstName={"Maria"}
-          lastName={"Smith"}
-          age={"62"}
-          hairColor={"Brown"}
-        />
+        {this.state.persons.map((person, index) => (
+          <PersonCard
+            key={index}
+            firstName={person.firstName}
+            lastName={person.lastName}
+            age={person.age}
+            hairColor={person.hairColor}
+            incrementarEdad={() => this.incrementarEdad(index)}
+          />
+        ))}
       </>
     );
   }
--- src/componentes/PersonCard/PersonCard.jsx
+++ src/componentes/PersonCard/PersonCard.jsx
@@ -1,11 +1,13 @@
-const PersonCard = (props) =>{
-    return(
-        <>
-            <h3> {props.lastName}, {props.firstName} </h3>
-            <p>Age: {props.age} </p>
-            <p>Hair Color {props.hairColor} </p>
-        </>
+const PersonCard = (props) => {
+    return (
+      <>
+        <h3>{props.lastName}, {props.firstName}</h3>
+        <p>Age: {props.age}</p>
+        <p>Hair Color: {props.hairColor}</p>
+        <button onClick={props.incrementarEdad}>Birthday button for {props.firstName}, {props.lastName}</button>
+      </>
     );
-}
-
-export default PersonCard;
+  }
+  
+  export default PersonCard;
+  
